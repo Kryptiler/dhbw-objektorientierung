779d6a8c3ae4e7d40f851733d639699b4033dea7
apostel.marius <apostel.marius@348A20>
2019-11-07 14:18:16 +0100

    Klassengerüst

--- Beispielprojekt/Maincharacter.cpp
+++ Beispielprojekt/Maincharacter.cpp
@@ -10,3 +10,21 @@ Maincharacter::Maincharacter()
 Maincharacter::~Maincharacter()
 {
 }
+
+void Maincharacter::bewege()
+{
+	x += v_x;
+	y += v_y;
+	animation = animation + 1 % 6;
+}
+
+void Maincharacter::right()
+{
+	v_x = 1;
+}
+
+void Maincharacter::left()
+{
+	v_x = -1;
+}
+
--- Beispielprojekt/Maincharacter.h
+++ Beispielprojekt/Maincharacter.h
@@ -4,8 +4,14 @@ class Maincharacter
 public:
 	Maincharacter();
 	~Maincharacter();
+	void bewege();
+	void right();
+	void left();
 private:
 	double x;
 	double y;
+	double v_x;
+	double v_y;
+	int animation;
 };
 
